dec31ae22fd31e8797be0013611c1c38fa9e3d7a
Oscar Tunebing <tunebing@gmail.com>
2019-06-18 13:00:15 +0200

    Change createUrl api to POST

--- src/api.js
+++ src/api.js
@@ -1,2 +1,8 @@
+"use strict";
+
 // Make API request (createUrl).
-export const createUrl = async (url) => fetch(`${API_URL}/createUrl/${encodeURI(url)}`).then(res => res.text());
+export const createUrl = async (url) => {
+    return fetch(`${API_URL}/api/createUrl/${encodeURIComponent(url)}`, {
+        method: 'POST',
+    });
+};
--- src/api.test.js
+++ src/api.test.js
@@ -2,7 +2,7 @@ import { createUrl } from './api';
 
 beforeAll(() => {
     global.API_URL = 'API_URL';
-    //global.REDIRECT_URL = 'REDIRECT_URL';
+    global.REDIRECT_URL = 'REDIRECT_URL';
     global.fetch = async () => {};
 });
 
@@ -10,6 +10,7 @@ beforeEach(() => {
     const mockJsonPromise = Promise.resolve({});
     const mockTextPromise = Promise.resolve('');
     const mockFetchPromise = Promise.resolve({
+        status: 200,
         text: () => mockTextPromise,
         json: () => mockJsonPromise,
     });
@@ -24,9 +25,9 @@ afterEach(() => {
 describe('createUrl', () => {
     it('should call fetch once with encoded url', async () => {
         const testUrl = 'https://duckduckgo.com/   123';
-        const expectedRequestUrl = `${API_URL}/createUrl/https://duckduckgo.com/%20%20%20123`;
+        const expectedRequestUrl = `API_URL/api/createUrl/https%3A%2F%2Fduckduckgo.com%2F%20%20%20123`;
         await createUrl(testUrl);
         expect(global.fetch).toHaveBeenCalledTimes(1);
-        expect(global.fetch).toHaveBeenCalledWith(expectedRequestUrl);
+        expect(global.fetch).toHaveBeenCalledWith(expectedRequestUrl, { method: 'POST' });
     });
 });
