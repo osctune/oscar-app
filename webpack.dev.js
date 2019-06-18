816fe98cf73392e2f26b788f1669602a7252d545
Oscar Tunebing <tunebing@gmail.com>
2019-06-18 12:59:24 +0200

    URL injection

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,24 @@
 const baseConfig = require('./webpack.common');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = Object.assign(baseConfig, {
+    entry: {
+        app: './src/index.js',
+    },
     mode: 'development',
     devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+        hot: true,
+    },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'OSCAR - development',
             template: 'src/ejs/index.dev.ejs',
+            API_URL: process.env.API_URL,
+            REDIRECT_URL: process.env.REDIRECT_URL,
         }),
+        new webpack.HotModuleReplacementPlugin(),
     ],
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,19 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
     plugins: [
+        new CopyPlugin([
+            { from: './public', to: './' },
+        ]),
         new HtmlWebpackPlugin({
             title: 'OSCAR',
             template: 'src/ejs/index.prod.ejs',
+            API_URL: process.env.API_URL,
+            REDIRECT_URL: process.env.REDIRECT_URL,
         }),
-    ],
+    ]
 });
