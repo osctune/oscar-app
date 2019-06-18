2f0a40e6c44648f7d8d7be846ec095516c48a8e5
Oscar Tunebing <tunebing@gmail.com>
2019-06-18 15:41:42 +0200

    Move uniqueBy to external package

--- src/util.js
+++ src/util.js
@@ -1,16 +1,2 @@
 // Remove item from list based on id.
 export const removeById = (list, action) => list.filter(item => item.id !== action.id);
-
-// Make list of unique items.
-export const uniqueBy = (list, getKey) => {
-    const set = new Set([]);
-    return list.filter(item => {
-        const key = getKey(item);
-        if (!set.has(key)) {
-            set.add(key);
-            return true;
-        } else {
-            return false;
-        }
-    });
-};
--- src/util.test.js
+++ src/util.test.js
@@ -1,4 +1,4 @@
-import { removeById, uniqueBy } from './util';
+import { removeById } from './util';
 
 describe('removeById', () => {
     it('should return a new list without items with the same id as the item provided', () => {
@@ -20,55 +20,5 @@ describe('removeById', () => {
             { id: 1, },
             { id: 3, },
         ]);
-    });
-});
-
-describe('uniqueBy', () => {
-    it('should return a new list containing only unique items', () => {
-        const items = [
-            { key: 'a', uid: '1', },
-            { key: 'a', uid: '2', },
-            { key: 'a', uid: '3', },
-            { key: 'b', uid: '4', },
-            { key: 'c', uid: '5', },
-        ];
-
-        const result = uniqueBy(items, o => o.key);
-
-        expect(result.map(o => o.key)).toStrictEqual(['a', 'b', 'c']);
-    });
-    it('should preserve order', () => {
-        const items = [
-            { key: 'a', uid: '1', },
-            { key: 'a', uid: '2', },
-            { key: 'a', uid: '3', },
-            { key: 'b', uid: '4', },
-            { key: 'c', uid: '5', },
-        ];
-
-        const result = uniqueBy(items, o => o.key);
-
-        expect(result.map(o => o.uid)).toStrictEqual(['1', '4', '5']);
-    });
-    it('should preserve input list', () => {
-        const items1 = [
-            { key: 'a', uid: '1', },
-            { key: 'a', uid: '2', },
-            { key: 'a', uid: '3', },
-            { key: 'b', uid: '4', },
-            { key: 'c', uid: '5', },
-        ];
-
-        const items2 = [
-            { key: 'a', uid: '1', },
-            { key: 'a', uid: '2', },
-            { key: 'a', uid: '3', },
-            { key: 'b', uid: '4', },
-            { key: 'c', uid: '5', },
-        ];
-
-        uniqueBy(items1, o => o.key);
-
-        expect(items2).toStrictEqual(items1);
     });
 });
